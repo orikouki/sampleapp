2155780d86f41cb2764fa881dbfd4fc1b1d6f088
tontomo <mingor@hotmail.co.jp>
2020-06-19 18:56:42 +0900

    test douusa

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,21 +5,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   @base_title = "Ruby on Rails Tutorial Sample App"
 end
 
-test "should get root" do
-  get root_url
-  assert_response :success
-end
 
-test "should get contact" do
-  get static_pages_contact_url
-  assert_response :success
-  assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
-end
 
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
@@ -32,5 +23,14 @@ end
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
 
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+  end
 end
