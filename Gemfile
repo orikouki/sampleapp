bb3898d08b79fa12f0f67d4300c089c3dd0e1541
tontomo <mingor@hotmail.co.jp>
2020-07-06 21:11:31 +0900

    Add user microposts

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem 'will_paginate',           '3.1.6'
 
 gem 'bootstrap-will_paginate', '1.0.0'
 
+gem 'carrierwave',             '1.2.2'
+gem 'mini_magick',             '4.10.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -65,6 +67,7 @@ end
 
 group :production do
   gem 'pg', '>=0.20.0'
+    gem 'fog', '1.42'
 end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   def show
     @user = User.find(params[:id])
      redirect_to root_url and return unless @user.activated?
+     @microposts = @user.microposts.paginate(page: params[:page])
 end
 
 def create
@@ -54,13 +55,6 @@ private
                                  :password_confirmation)
   end
 
-  def logged_in_user
-  unless logged_in?
-      store_location
-    flash[:danger] = "Please log in."
-    redirect_to login_url
-  end
-end
 
 def correct_user
   @user = User.find(params[:id])
