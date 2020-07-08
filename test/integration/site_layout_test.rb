950751ebde4ec4d3c9b6c2fb106a3d84856c1bdb
tontomo <mingor@hotmail.co.jp>
2020-07-08 19:17:17 +0900

    Add user following

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,6 +4,10 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
   get root_path
   assert_template 'static_pages/home'
@@ -15,5 +19,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
  assert_select "title", full_title("Contact")
  get signup_path
   assert_select "title", full_title("Sign up")
+end
+
+test "count relationships" do
+  log_in_as(@user)
+  get root_path
+  assert_match @user.active_relationships.count.to_s, response.body
+  assert_match @user.passive_relationships.count.to_s, response.body
 end
 end
